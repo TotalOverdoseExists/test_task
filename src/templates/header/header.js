756c706acbb404320ea025635fc12ca4a1ed8271
TotalOverdoseExists <verylonghair00@gmail.com>
2022-09-07 17:32:03 +0500

    Desktop header start

--- src/templates/header/header.js
+++ src/templates/header/header.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import './header.scss'
+import logo from '../../images/logo.png'
+
+const menuItems = {
+	'/link/': 'text',
+	'/link1/': 'text_text',
+	'/link11/': 'text_text_text'
+}
 
 function Header() {
 	return (
-		<header></header>
+		<header>
+			<div className='container'>
+				<figure>
+					<img src={logo} alt='logo' />
+					<figcaption>Logo</figcaption>
+				</figure>
+				<nav>
+					<ul>
+						{Object.entries(menuItems).map(([link, item]) => (
+							<li key={item}>
+								<a href={link}>{item}</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+			</div>
+		</header>
 	)
 }
 
