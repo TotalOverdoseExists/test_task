ff873e056d9a801c424329727a48d98f5b8d22ae
TotalOverdoseExists <verylonghair00@gmail.com>
2022-09-09 14:51:50 +0500

    Header done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-utilities.css'
 import './index.scss'
 import Header from './templates/header/header'
 import Main from './templates/main/main'
--- src/templates/header/header.js
+++ src/templates/header/header.js
@@ -9,22 +9,35 @@ const menuItems = {
 }
 
 function Header() {
+	const menuToggle = (e) => {
+		e.preventDefault()
+		document.querySelector('#l-menu').classList.toggle('js-menuOpen')
+	}
+
 	return (
-		<header>
+		<header id='header'>
 			<div className='container'>
-				<figure>
-					<img src={logo} alt='logo' />
-					<figcaption>Logo</figcaption>
-				</figure>
-				<nav>
-					<ul>
-						{Object.entries(menuItems).map(([link, item]) => (
-							<li key={item}>
-								<a href={link}>{item}</a>
-							</li>
-						))}
-					</ul>
-				</nav>
+				<div className='row align-items-center align-items-lg-end mx-md-0'>
+					<div className='col-4 col-md-auto'>
+						<img src={logo} alt='logo' />
+					</div>
+					<div className='col-4 col-md-auto col-lg text-center text-md-start'>
+						<span>Logo</span>
+					</div>
+					<div className='col-4 d-md-none text-end'>
+						<button onClick={menuToggle}>&#9776;</button>
+					</div>
+					<nav id='l-menu' className='col-md-9 col-lg-6 text-end text-md-center ms-md-auto'>
+						<button onClick={menuToggle} className='d-md-none'>&#10006;</button>
+						<ul>
+							{Object.entries(menuItems).map(([link, item]) => (
+								<li key={item}>
+									<a href={link}>{item}</a>
+								</li>
+							))}
+						</ul>
+					</nav>
+				</div>
 			</div>
 		</header>
 	)
