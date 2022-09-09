f41cd6804bec8e85cceec8cf97c6cd3aae07cd4d
TotalOverdoseExists <verylonghair00@gmail.com>
2022-09-09 17:53:17 +0500

    Main banner start, found mobile menu bug

--- src/templates/main/main.js
+++ src/templates/main/main.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './main.scss'
 
+function Banner() {
+	return (
+		<section id='l-banner'>
+			<div className='container'>
+				<div className='row text-center text-md-start'>
+					<div className='col-12'>
+						<h1>Text_text_text</h1>
+						<span>Text_text_text_text</span>
+					</div>
+					<div className='col-12 col-md-auto'>
+						<button>Text_text_text</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
 function Main() {
 	return (
-		<main></main>
+		<main>
+			<Banner />
+		</main>
 	)
 }
 
